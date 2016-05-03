61a41c23df8e677a7e1883991faabdbe20bac70d
Frank Liu Mac <frankliu81@gmail.com>
2016-05-03 13:14:52 -0700

    AJAXify question like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,9 @@ class LikesController < ApplicationController
   before_action :authenticate_user!
   before_action :authorize_create, only: :create
   before_action :authorize_destroy, only: :destroy
+  before_action :question # this will call the question method to force
+                          # finding a question as we will need it for both
+                          # the create and destroy actions
 
   def create
     # question refactored below
@@ -9,12 +12,18 @@ class LikesController < ApplicationController
     like = Like.new
     like.user = current_user
     like.question = question
-    if like.save
-      #redirect_to question_path(@question)
-      # question is a short form for question_path(@quesiton)
-      redirect_to question, notice: "Liked!"
-    else
-      redirect_to question, alert: "You've Already Liked!"
+
+    respond_to do |format|
+      if like.save
+        #redirect_to question_path(@question)
+        # question is a short form for question_path(@quesiton)
+        format.html { redirect_to question, notice: "Liked!" }
+        format.js { render } # likes/create.js.erb
+      else
+        format.html { redirect_to question, alert: "You've Already Liked!"}
+        # user click too quickly
+        format.js { render js: "alert('Can\'t like, please refresh the page!');" }
+      end
     end
 
   end
@@ -29,9 +38,12 @@ class LikesController < ApplicationController
     #like = Like.find params[:id]
 
     like.destroy
-    #redirect_to question, notice: "Un-liked!"
-    # greg' suggested to use like.question_id directly
-    redirect_to question_path(like.question_id), notice: "Un-liked!"
+    respond_to do |format|
+      #redirect_to question, notice: "Un-liked!"
+      # greg' suggested to use like.question_id directly
+      format.html { redirect_to question_path(like.question_id), notice: "Un-liked!"}
+      format.js { render }
+    end
   end
 
   private
